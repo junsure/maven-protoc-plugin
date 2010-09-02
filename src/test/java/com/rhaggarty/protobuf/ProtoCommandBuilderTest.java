2ce0cac5e14daff1d427604047e891ed5e74f0d0
rhaggarty <rhaggarty@localhost>
2010-09-02 19:52:34 +0200

    Added test for ProtoCommandBuilder

--- src/test/java/com/rhaggarty/protobuf/ProtoCommandBuilderTest.java
+++ src/test/java/com/rhaggarty/protobuf/ProtoCommandBuilderTest.java
@@ -1,11 +1,19 @@
 package com.rhaggarty.protobuf;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
 import java.io.File;
+import java.io.FileFilter;
+import java.util.Collections;
 import java.util.Map;
 
+import org.codehaus.plexus.util.cli.Commandline;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 
@@ -13,21 +21,33 @@ public class ProtoCommandBuilderTest {
 
     private static final String EXECUTABLE = "./this/is/not/a/real/executable";
     
-    private Map<ProtoOutputType, String> outputTypes;
-    
     @Mock private File sourceDirectory;
     
     private ProtoCommandBuilder builder;
     
     @Before
     public void setup() {
-        MockitoAnnotations.initMocks(this.getClass());
+        MockitoAnnotations.initMocks(this);
         builder = new ProtoCommandBuilder();
     }
     
     @Test
     public void testBuildProtoCommand() {
-        //builder.buildProtoCommand(sourceDirectory, outputTypes, EXECUTABLE);
+        final Map<ProtoOutputType, String> outputTypes = Collections.singletonMap(ProtoOutputType.JAVA, ".");
+        final File mockFile = Mockito.mock(File.class);
         
+        when(sourceDirectory.isDirectory()).thenReturn(true);
+        when(sourceDirectory.listFiles(any(FileFilter.class))).thenReturn(new File[] {mockFile});
+        when(sourceDirectory.toString()).thenReturn(".");
+        when(mockFile.toString()).thenReturn("mock-file.proto");
+        
+        final Commandline command = builder.buildProtoCommand(sourceDirectory, outputTypes, EXECUTABLE);
+        
+        final StringBuilder sb = new StringBuilder("/bin/sh -c ");
+        sb.append(EXECUTABLE).append(" ").append(mockFile).append(" ");
+        sb.append("--java_out=. --proto_path=.");
+         
+        Assert.assertEquals(sb.toString(), command.toString());
+        System.out.println(command);
     }   
 }
